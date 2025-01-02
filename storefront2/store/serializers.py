cf9d9716f2871761a99e2264fca13dd8cb60393d
bikramia <bikram.rhce@gmail.com>
2025-01-02 07:19:17 -0500

    Nested Routers -still trying to figure out the errors

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -3,26 +3,29 @@ from decimal import Decimal
 from store.models import Product, Collection, Review
 
 class CollectionSerializer(serializers.ModelSerializer):
-    class Meta: 
+    class Meta:
         model = Collection
         fields = ['id', 'title', 'products_count']
 
     products_count = serializers.IntegerField(read_only=True)
 
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    
+
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
-    
-class ReviewSerializer(serializers.Serializer):
+
+   
+class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ['id', 'date', 'name', 'description' 'product']
+        fields = ['id', 'date', 'name', 'description']
 
     def create(self, validated_data):
         product_id = self.context['product_id']
--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -15,30 +15,38 @@ from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
 
-    def destroy(self, request, *args, **kwargs):
-        if OrderItem.objects.filter(product_id=self.kwargs['pk']).count() > 0:
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        if product.orderitems.count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        
-        return super().destroy(request, *args, **kwargs)
-    
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(
         products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
-    def destroy(self, request, *args, **kwargs):
-        if Collection.objects.filter(products__id=self.kwargs['pk']).count() > 0:
+    def delete(self, request, pk):
+        collection = get_object_or_404(Collection, pk=pk)
+        if collection.products.count() > 0:
             return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        return super().destroy(request, *args, **kwargs)
-    
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
