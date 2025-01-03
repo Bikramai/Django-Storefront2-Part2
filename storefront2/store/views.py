1a303deba6c63f7ad630f906f07ccebf08faad05
bikramia <bikram.rhce@gmail.com>
2025-01-03 15:29:43 -0500

    Generic Filtering-with unit price

--- storefront2/store/views.py
+++ storefront2/store/views.py
@@ -12,16 +12,14 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
+from .filters import ProductFilter
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['collection_id']
-
-    
-    
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {'request': self.request}
