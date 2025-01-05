e09e2ed6eb85370df1175ebccf7a4bb67e77ccc7
bikramia <bikram.rhce@gmail.com>
2025-01-05 10:16:34 -0500

    Revisiting the Data Model-implemented UUID4 in Cart model

--- storefront2/store/models.py
+++ storefront2/store/models.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from django.core.validators import MinValueValidator
 from django.db import models
 
@@ -95,6 +96,7 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
