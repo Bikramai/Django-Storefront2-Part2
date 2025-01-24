9ef6ef6566f5a8394c11a7192ab74fe6ca9d6292
bikramia <bikram.rhce@gmail.com>
2025-01-24 17:11:11 -0500

    Creating a completely new database by droping storefront2 db

--- storefront2/core/admin.py
+++ storefront2/core/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
 from tags.models import TaggedItem
-from core.models import User
+from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
