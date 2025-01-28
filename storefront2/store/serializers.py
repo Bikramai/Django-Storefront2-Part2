9401c31801cb2c65c35917bb5980171829edce91
bikramia <bikram.rhce@gmail.com>
2025-01-28 07:20:23 -0500

    Creating an order-Some thing is not working right -"detail": "Authentication credentials were not provided."

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -134,37 +134,44 @@ class UpdateOrderSerializer(serializers.ModelSerializer):
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
-    def validate_cart_id(self, cart_id):
-        if not Cart.objects.filter(pk=cart_id).exists():
-            raise serializers.ValidationError(
-                'No cart with the given ID was found.')
-        if CartItem.objects.filter(cart_id=cart_id).count() == 0:
-            raise serializers.ValidationError('The cart is empty.')
-        return cart_id
-
     def save(self, **kwargs):
-        with transaction.atomic():
-            cart_id = self.validated_data['cart_id']
+        print (self.validated_datat['cart_id'])
+        print(self.contest['user_id'])
 
-            customer = Customer.objects.get(
-                user_id=self.context['user_id'])
-            order = Order.objects.create(customer=customer)
+        (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
+        Order.objects.create(customer=customer)
 
-            cart_items = CartItem.objects \
-                .select_related('product') \
-                .filter(cart_id=cart_id)
-            order_items = [
-                OrderItem(
-                    order=order,
-                    product=item.product,
-                    unit_price=item.product.unit_price,
-                    quantity=item.quantity
-                ) for item in cart_items
-            ]
-            OrderItem.objects.bulk_create(order_items)
+    # def validate_cart_id(self, cart_id):
+    #     if not Cart.objects.filter(pk=cart_id).exists():
+    #         raise serializers.ValidationError(
+    #             'No cart with the given ID was found.')
+    #     if CartItem.objects.filter(cart_id=cart_id).count() == 0:
+    #         raise serializers.ValidationError('The cart is empty.')
+    #     return cart_id
 
-            Cart.objects.filter(pk=cart_id).delete()
+    # def save(self, **kwargs):
+    #     with transaction.atomic():
+    #         cart_id = self.validated_data['cart_id']
 
-            order_created.send_robust(self.__class__, order=order)
+    #         customer = Customer.objects.get(
+    #             user_id=self.context['user_id'])
+    #         order = Order.objects.create(customer=customer)
 
-            return order
+    #         cart_items = CartItem.objects \
+    #             .select_related('product') \
+    #             .filter(cart_id=cart_id)
+    #         order_items = [
+    #             OrderItem(
+    #                 order=order,
+    #                 product=item.product,
+    #                 unit_price=item.product.unit_price,
+    #                 quantity=item.quantity
+    #             ) for item in cart_items
+    #         ]
+    #         OrderItem.objects.bulk_create(order_items)
+
+    #         Cart.objects.filter(pk=cart_id).delete()
+
+    #         order_created.send_robust(self.__class__, order=order)
+
+    #         return order
