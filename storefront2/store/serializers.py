c28a7f26d16d02834d7c779fe568b88731fa9422
bikramia <bikram.rhce@gmail.com>
2025-01-29 20:43:22 -0500

    Creating order Items-today no time I will fix next time

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -135,11 +135,11 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print (self.validated_datat['cart_id'])
-        print(self.contest['user_id'])
 
         (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
         Order.objects.create(customer=customer)
+
+        cart_items = CartItem.objects.filter(cart_id=self.validated_data['cart_id'])
 
     # def validate_cart_id(self, cart_id):
     #     if not Cart.objects.filter(pk=cart_id).exists():
